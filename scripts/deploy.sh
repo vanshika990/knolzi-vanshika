dc819c58391848adf2be3a182a8af54fa916163a
Ubuntu <ubuntu@ip-172-31-36-249.ec2.internal>
2026-05-20 10:13:53 +0000

    Added CodeDeploy configuration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,19 @@
 
 cd /var/www/html/knolzi-vanshika
 
-echo "Deployment started"
+echo "Deployment Started"
 
-composer install --no-interaction --prefer-dist --optimize-autoloader
+sudo chown -R ubuntu:ubuntu /var/www/html/knolzi-vanshika
+
+composer install --no-interaction
 
 php artisan optimize:clear
 
 php artisan config:cache
 
-php artisan route:cache
-
-sudo chown -R www-data:www-data storage bootstrap/cache
-
 sudo chmod -R 775 storage bootstrap/cache
 
 sudo systemctl restart apache2
 
-echo "Deployment completed"
+echo "Deployment Finished"
+
